6daf81fbc4225a47eb6c912161e80f2f761a7310
jimo <1044818771@qq.com>
2023-04-18 08:40:54 +0800

    feat:数组劫持，部分方法重写

---
+++ src/observe/array.js
@@ -0,0 +1,32 @@
+const oldArrayProto = Array.prototype; // 保存Array上已有的原型方法
+export const newArrayProto = Object.create(oldArrayProto); // 通过已有的原型方法，创建一个对象
+
+const methods = ['push', 'pop', 'shift', 'unshift', 'sort', 'reverse', 'splice']; // 记录会改变数组的原型方法名称
+
+methods.forEach(method => {
+    // 重写对象中的方法
+    newArrayProto[method] = function (...args) {
+        console.log('newArrayProto', method);
+        // 调用原来的方法对数组进行操作（注意this指向）
+        const result = oldArrayProto[method].call(this, ...args);
+        let inserted = null;
+        let ob = this.__ob__; // 获取调用者身上保存的Observe实例
+        switch (method) {
+            case 'push':
+            case 'unshift':
+                inserted = args;
+                break;
+            case 'splice': 
+                inserted = args.slice(2);
+                break;
+            default: 
+                break;
+        }
+        if (inserted) {
+            ob.observeArray(inserted);
+        }
+        // 对改变数组的值进行劫持
+        return result;
+    }
+})
+
--- src/observe/index.js
+++ src/observe/index.js
@@ -1,10 +1,26 @@
+import { newArrayProto } from './array';
+
 class Observe {
     constructor(data) {
+        // 在data属性上保存实例，以便后续随时可以调用walk/observeArray等方法
+        Object.defineProperty(data, '__ob__', { 
+            vaule: this,
+            enumerable: false, // 不可被枚举（遍历），方死循环
+        });
+        if (Array.isArray(data)) {
+            // 劫持数组每个属性极其耗费性能，所以直接修改会对数组造成改动的方法
+            data.__proto__ = newArrayProto; // 改变data中数组变量的原型（影响性能）
+            this.observeArray(data); // 
+        }
         this.walk(data);
     }
     walk(data) {
         // 遍历data的所有属性，依次劫持
         Object.keys(data).forEach(key => defineReactive(data, key, data[key]));
+    }
+    observeArray(data) {
+        // 劫持数组的属性
+        data.forEach(item => observe(item));
     }
 }
 
@@ -17,13 +33,14 @@ export function defineReactive(target, key, value) { // 形成闭包
         },
         set(newVal) {
             if (value === newVal) return;
+            observe(newVal); // 如果设置新值为对象，则需要继续劫持
             value = newVal;
         }
     })
 }
 
 export function observe(data) {
-    // 只劫持对象
+    // 只劫持对象，多层对象深度劫持时的出口
     if (typeof data !== 'object') return;
     return new Observe(data);
 }
